f5ed9545ea4a7781c3f23567036a8e26e742643a
DuArms <58860510+DuArms@users.noreply.github.com>
2020-12-02 22:39:58 +0200

    Nou : Logica jocului si AI USOR

--- GameLogic.py
+++ GameLogic.py
@@ -10,7 +10,7 @@ class GameLogic:
     MSG_LOST = "MOUSE_WIN"
     MSG_WON = "WALL_WIN"
 
-    def __init__(self, crd_soarece=(NUMAR_LINII // 2 , NUMAR_COLOANE // 2)):
+    def __init__(self, crd_soarece=(NUMAR_LINII // 2, NUMAR_COLOANE // 2)):
         self.tabla = np.zeros((NUMAR_LINII, NUMAR_COLOANE))
         self.crd_soarece = np.array(crd_soarece)
         self.tabla[crd_soarece] = GameLogic.SOARECE
@@ -29,19 +29,19 @@ class GameLogic:
         for x, y in zip(move_x, move_y):
             x, y = self.crd_soarece + np.asarray([x, y])
 
-            if self.valid_move(x, y):
+            if self.valid_move(x, y, [GameLogic.SPATIU,GameLogic.VIZITAT]):
                 valid_move.append((x, y))
 
         return valid_move
 
-    def valid_move(self, x, y):
-        if 0 <= x < NUMAR_LINII and 0 <= y < NUMAR_COLOANE and self.tabla[x, y] in [GameLogic.SPATIU,GameLogic.VIZITAT] :
+    def valid_move(self, x, y, poz_libere=None):
+        if 0 <= x < NUMAR_LINII and 0 <= y < NUMAR_COLOANE and self.tabla[x, y] in poz_libere:
             return True
         return False
 
     def move_mouse(self, x, y):
-        if self.valid_move(x, y):
-            self.tabla[self.crd_soarece[0] , self.crd_soarece[1]] = GameLogic.VIZITAT
+        if self.valid_move(x, y ,[GameLogic.SPATIU,GameLogic.VIZITAT]):
+            self.tabla[self.crd_soarece[0], self.crd_soarece[1]] = GameLogic.VIZITAT
             self.crd_soarece = np.asarray((x, y))
             self.tabla[x, y] = GameLogic.SOARECE
 
@@ -49,20 +49,20 @@ class GameLogic:
         return False
 
     def build_wall(self, x, y):
-        if self.valid_move(x, y):
+        if self.valid_move(x, y, [GameLogic.VIZITAT, GameLogic.SPATIU]):
             self.tabla[x, y] = GameLogic.ZID
             return True
         return False
 
-    def is_game_done(self,numar_mutari):
+    def is_game_done(self, numar_mutari):
         if self.crd_soarece[0] in [0, NUMAR_LINII - 1] or self.crd_soarece[1] in [0, NUMAR_COLOANE - 1]:
             return GameLogic.MSG_LOST
         if numar_mutari == 0:
-            return  GameLogic.MSG_WON
+            return GameLogic.MSG_WON
 
-        return  None
+        return None
 
-    def random_logic(self,mutari_posibile,nr_mutari):
+    def random_logic(self, mutari_posibile, nr_mutari):
 
         mutare_id = np.random.randint(0, nr_mutari)
         mutare = mutari_posibile[mutare_id]
@@ -75,11 +75,12 @@ def play_ai(game_state=GameLogic(), diff_lvl=1):
     nr_mutari = len(moves)
 
     game_done = game_state.is_game_done(nr_mutari)
+
     if game_done is not None:
-        return  game_done
+        return game_done
 
     if diff_lvl == 1:
-        x, y = game_state.random_logic(moves , nr_mutari)
+        x, y = game_state.random_logic(moves, nr_mutari)
         game_state.move_mouse(x, y)
 
     return x, y
